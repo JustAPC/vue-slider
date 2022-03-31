483cba01288adc9ef8f38f6b7cfe971d73e0e26d
Andrea Pontillo <andreapontillo97@gmail.com>
2022-03-31 17:40:40 +0200

    stesura esercizio base

--- main.js
+++ main.js
@@ -3,32 +3,30 @@ var app = new Vue(
         el: "#root",
         data: {
             immagini: [
-                {
-                    'id' : 1,
-                    'img' : 'https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-                    'shown' : true
-                },
-                {
-                    'id' : 2,
-                    'img' : 'https://images.pexels.com/photos/210186/pexels-photo-210186.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-                    'shown' : false
-                },
-                {
-                    'id' : 3,
-                    'img' : 'https://images.pexels.com/photos/6992/forest-trees-northwestisbest-exploress.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-                    'shown' : false
-                }
+                'https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                'https://images.pexels.com/photos/210186/pexels-photo-210186.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                'https://images.pexels.com/photos/6992/forest-trees-northwestisbest-exploress.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                'https://images.pexels.com/photos/2440024/pexels-photo-2440024.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
             ],
-            counter : 1
+            index : 0
         },
 
+
         methods: {
-            rightSlide : function () {
-                
+            rightSlide () {
+                if (this.index < this.immagini.length) {
+                    this.index++
+                } else {
+                    this.index = 0;
+                }
             },
 
-            leftSlide : function () {
-
+            leftSlide () {
+                if (this.index > 0) {
+                    this.index--
+                } else {
+                    this.index = this.immagini.length -1;
+                }
             }
         },
     }
